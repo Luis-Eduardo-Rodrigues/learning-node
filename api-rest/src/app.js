90e920590abd30258fecc59d2de7882c61457c9e
Luis Eduardo Rodrigues <luiseduardorodrigues112@gmail.com>
2025-01-29 18:37:53 -0300

    routes get

---
+++ api-rest/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+
+// mock
+const selecoes = [
+  { id: 1, selecao: "Brasil", grupo: "G" },
+  { id: 2, selecao: "Suiça", grupo: "G" },
+  { id: 3, selecao: "Sérvia", grupo: "G" },
+  { id: 4, selecao: "Camarões", grupo: "G" },
+];
+
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World!" });
+});
+
+app.get("/selecoes", (req, res) => {
+  res.status(200).send(selecoes);
+});
+
+export default app;
